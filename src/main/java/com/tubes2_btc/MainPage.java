6bcf3b05c8b25616d19509b12fa137d317898aa9
trimonuter <mrqaist@gmail.com>
2024-05-12 13:27:02 +0700

    feat: create main_page

---
+++ src/main/java/com/tubes2_btc/MainPage.java
@@ -0,0 +1,24 @@
+package com.tubes2_btc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainPage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainPage.class.getResource("main_page.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
