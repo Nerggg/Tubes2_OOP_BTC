943b319bc508ac2d39f4d4bc7596578da75b065a
trimonuter <mrqaist@gmail.com>
2024-05-14 10:48:29 +0700

    feat: card class

---
+++ src/main/java/com/tubes2_btc/Components/CardController.java
@@ -0,0 +1,26 @@
+//package com.tubes2_btc.Components;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Label;
+//import javafx.scene.image.ImageView;
+//
+//public class CardController {
+//    @FXML
+//    private ImageView cardImage;
+//
+//    @FXML
+//    private Label cardName;
+//
+//    public CardController(String imageURL, String name) {
+//        SetImage(imageURL);
+//        SetName(name);
+//    }
+//
+//    public void SetImage(String imageURL) {
+//        cardImage.setImage(new javafx.scene.image.Image(imageURL));
+//    }
+//
+//    public void SetName(String name) {
+//        cardName.setText(name);
+//    }
+//}
