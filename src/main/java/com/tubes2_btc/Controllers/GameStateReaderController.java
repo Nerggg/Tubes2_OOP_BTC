702523f3b7779e96181b3708c6908798c15da349
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2024-05-21 15:04:34 +0700

    add: game state reader

--- src/main/java/com/tubes2_btc/Controllers/GameStateReaderController.java
+++ src/main/java/com/tubes2_btc/Controllers/GameStateReaderController.java
@@ -10,8 +10,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TxtController {
+public class GameStateReaderController {
     @FXML
     private Button uploadButton;
 
@@ -31,7 +33,30 @@ public class TxtController {
         if (selectedFile != null) {
             try {
                 String content = Files.readString(Path.of(selectedFile.toURI()));
+                String[] lines = content.split("\\R");
                 textArea.setText(content);
+
+                for (String line : lines){
+                    System.out.println(line);
+                }
+
+                if (lines.length > 1){
+                    int currentTurn = Integer.parseInt(lines[0]);
+                    int jumlahItemDiShop = Integer.parseInt(lines[1]);
+
+                    List<String> itemShop = new ArrayList<>(jumlahItemDiShop);
+
+                    for (int i = 0; i < jumlahItemDiShop; i++){
+                        itemShop.add(lines[2+i]);
+                    }
+
+                    System.out.println("CURRENT TURN: " + currentTurn);
+                    System.out.println("JUMLAH ITEM DI SHOP: " + jumlahItemDiShop);
+                    System.out.println("ITEM SHOP: " + itemShop);
+
+                }
+
+
             } catch (IOException e) {
                 textArea.setText("Failed to load the file.");
             }
