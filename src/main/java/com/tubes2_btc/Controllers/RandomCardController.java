e5744b85662bb355187086d784ec3f0b14184adc
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2024-05-22 13:56:33 +0700

    fix: image not load at shuffle card

--- src/main/java/com/tubes2_btc/Controllers/RandomCardController.java
+++ src/main/java/com/tubes2_btc/Controllers/RandomCardController.java
@@ -34,6 +34,46 @@ public class RandomCardController {
 
     @FXML
     private void initialize() {
+        int random1 = random.nextInt(1, 25);
+        Card card1 = CardConstants.createCard(random1);
+
+        URL imageUrl1 = getClass().getResource(card1.getCardPath());
+        if (imageUrl1 != null) {
+            Image imageNew1 = new Image(imageUrl1.toExternalForm());
+            imageView1.setImage(imageNew1);
+            label1.setText(card1.getCardName());
+
+        }
+
+        int random2 = random.nextInt(1, 25);
+        Card card2 = CardConstants.createCard(random2);
+
+        URL imageUrl2 = getClass().getResource(card2.getCardPath());
+        if (imageUrl2 != null){
+            Image imageNew2 = new Image(imageUrl2.toExternalForm());
+            imageView2.setImage(imageNew2);
+            label2.setText(card2.getCardName());
+        }
+
+        int random3 = random.nextInt(1, 25);
+        Card card3 = CardConstants.createCard(random3);
+
+        URL imageUrl3 = getClass().getResource(card3.getCardPath());
+        if (imageUrl3 != null){
+            Image imageNew3 = new Image(imageUrl3.toExternalForm());
+            imageView3.setImage(imageNew3);
+            label3.setText(card3.getCardName());
+        }
+
+        int random4 = random.nextInt(1, 25);
+        Card card4 = CardConstants.createCard(random4);
+
+        URL imageUrl4 = getClass().getResource(card4.getCardPath());
+        if (imageUrl4 != null){
+            Image imageNew4 = new Image(imageUrl4.toExternalForm());
+            imageView4.setImage(imageNew4);
+            label4.setText(card4.getCardName());
+        }
         randomizeButton.setOnAction(event -> changeImages());
     }
 
