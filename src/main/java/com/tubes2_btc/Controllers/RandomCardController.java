9f453e92472c68d43ec5908f31ebd909cd3ebe63
trimonuter <mrqaist@gmail.com>
2024-05-22 13:06:49 +0700

    refactor: call createCard in RandomCardController

--- src/main/java/com/tubes2_btc/Controllers/RandomCardController.java
+++ src/main/java/com/tubes2_btc/Controllers/RandomCardController.java
@@ -1,6 +1,6 @@
 package com.tubes2_btc.Controllers;
 
-import com.tubes2_btc.Classes.CardConstants;
+import com.tubes2_btc.Classes.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -30,60 +30,6 @@ public class RandomCardController {
     @FXML
     private Button randomizeButton;
 
-    private final String[] paths = {
-            CardConstants.CARD_HIU_PATH,
-            CardConstants.CARD_SAPI_PATH,
-            CardConstants.CARD_DOMBA_PATH,
-            CardConstants.CARD_KUDA_PATH,
-            CardConstants.CARD_AYAM_PATH,
-            CardConstants.CARD_BERUANG_PATH,
-            CardConstants.CARD_BIJI_LABU_PATH,
-            CardConstants.CARD_BIJI_JAGUNG_PATH,
-            CardConstants.CARD_BIJI_STROBERI_PATH,
-            CardConstants.CARD_LABU_PATH,
-            CardConstants.CARD_JAGUNG_PATH,
-            CardConstants.CARD_STROBERI_PATH,
-            CardConstants.CARD_SUSU_PATH,
-            CardConstants.CARD_TELUR_PATH,
-            CardConstants.CARD_SIRIP_HIU_PATH,
-            CardConstants.CARD_DAGING_KUDA_PATH,
-            CardConstants.CARD_DAGING_DOMBA_PATH,
-            CardConstants.CARD_DAGING_BERUANG_PATH,
-            CardConstants.CARD_ACCELERATE_PATH,
-            CardConstants.CARD_DELAY_PATH,
-            CardConstants.CARD_INSTANT_HARVEST_PATH,
-            CardConstants.CARD_DESTROY_PATH,
-            CardConstants.CARD_PROTECT_PATH,
-            CardConstants.CARD_TRAP_PATH
-    };
-
-    private final String[] paths_name = {
-            CardConstants.CARD_HIU,
-            CardConstants.CARD_SAPI,
-            CardConstants.CARD_DOMBA,
-            CardConstants.CARD_KUDA,
-            CardConstants.CARD_AYAM,
-            CardConstants.CARD_BERUANG,
-            CardConstants.CARD_BIJI_LABU,
-            CardConstants.CARD_BIJI_JAGUNG,
-            CardConstants.CARD_BIJI_STROBERI,
-            CardConstants.CARD_LABU,
-            CardConstants.CARD_JAGUNG,
-            CardConstants.CARD_STROBERI,
-            CardConstants.CARD_SUSU,
-            CardConstants.CARD_TELUR,
-            CardConstants.CARD_SIRIP_HIU,
-            CardConstants.CARD_DAGING_KUDA,
-            CardConstants.CARD_DAGING_DOMBA,
-            CardConstants.CARD_DAGING_BERUANG,
-            CardConstants.CARD_ACCELERATE,
-            CardConstants.CARD_DELAY,
-            CardConstants.CARD_INSTANT_HARVEST,
-            CardConstants.CARD_DESTROY,
-            CardConstants.CARD_PROTECT,
-            CardConstants.CARD_TRAP
-    };
-
     private final Random random = new Random();
 
     @FXML
@@ -92,36 +38,45 @@ public class RandomCardController {
     }
 
     private void changeImages() {
-        int random1 = random.nextInt(0, paths.length-1);
-        URL imageUrl1 = getClass().getResource(paths[random1]);
+        int random1 = random.nextInt(1, 25);
+        Card card1 = CardConstants.createCard(random1);
+
+        URL imageUrl1 = getClass().getResource(card1.getCardPath());
         if (imageUrl1 != null) {
             Image imageNew1 = new Image(imageUrl1.toExternalForm());
             imageView1.setImage(imageNew1);
-            label1.setText(paths_name[random1]);
+            label1.setText(card1.getCardName());
 
         }
-        int random2 = random.nextInt(0, paths.length-1);
-        URL imageUrl2 = getClass().getResource(paths[random2]);
+        
+        int random2 = random.nextInt(1, 25);
+        Card card2 = CardConstants.createCard(random2);
+
+        URL imageUrl2 = getClass().getResource(card2.getCardPath());
         if (imageUrl2 != null){
             Image imageNew2 = new Image(imageUrl2.toExternalForm());
             imageView2.setImage(imageNew2);
-            label2.setText(paths_name[random2]);
+            label2.setText(card2.getCardName());
         }
 
-        int random3 = random.nextInt(0, paths.length-1);
-        URL imageUrl3 = getClass().getResource(paths[random3]);
+        int random3 = random.nextInt(1, 25);
+        Card card3 = CardConstants.createCard(random3);
+
+        URL imageUrl3 = getClass().getResource(card3.getCardPath());
         if (imageUrl3 != null){
             Image imageNew3 = new Image(imageUrl3.toExternalForm());
             imageView3.setImage(imageNew3);
-            label3.setText(paths_name[random3]);
+            label3.setText(card3.getCardName());
         }
 
-        int random4 = random.nextInt(0, paths.length-1);
-        URL imageUrl4 = getClass().getResource(paths[random4]);
+        int random4 = random.nextInt(1, 25);
+        Card card4 = CardConstants.createCard(random4);
+
+        URL imageUrl4 = getClass().getResource(card4.getCardPath());
         if (imageUrl4 != null){
             Image imageNew4 = new Image(imageUrl4.toExternalForm());
             imageView4.setImage(imageNew4);
-            label4.setText(paths_name[random4]);
+            label4.setText(card4.getCardName());
         }
 //        imageView2.setImage(new Image(paths[random.nextInt(0, paths.length-1)]));
 //        imageView3.setImage(new Image(paths[random.nextInt(0, paths.length-1)]));
