b632dbf04d2690df8ced59eb00ab8b6af6f5f09a
nerggg <13522147@std.stei.itb.ac.id>
2024-05-23 17:02:10 +0700

    add: plugin functionality

--- src/main/java/com/tubes2_btc/Controllers/LoadPluginController.java
+++ src/main/java/com/tubes2_btc/Controllers/LoadPluginController.java
@@ -1,10 +1,19 @@
 package com.tubes2_btc.Controllers;
 
+import com.tubes2_btc.Classes.DataPasser;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class LoadPluginController {
 
@@ -12,8 +21,42 @@ public class LoadPluginController {
     private Button kembali;
 
     @FXML
-    private void handleClicked(ActionEvent event) {
+    private Button choose;
+
+    @FXML
+    private Label success;
+
+    @FXML
+    private Label fileName;
+
+    @FXML
+    private Button upload;
+
+    private File jarFile;
+
+    @FXML
+    private void handleClickedKembali(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    private void handleClickedChoose(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JAR files (*.jar)", "*.jar"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        File jarFile = fileChooser.showOpenDialog(stage);
+
+        if (jarFile != null) {
+            fileName.setText(jarFile.getName());
+            this.jarFile = jarFile;
+        }
+    }
+
+    @FXML
+    private void handleClickedUpload(ActionEvent event) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        DataPasser dataPasser = DataPasser.getInstance();
+        success.setText("Upload plugin berhasil!");
+        dataPasser.jarFile = this.jarFile;
     }
 }
