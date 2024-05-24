c442a5306f0fbcdd5c25bbda2ac9bb1b7ab1046d
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2024-05-25 01:47:44 +0700

    add: icon and title

--- src/main/java/com/tubes2_btc/App.java
+++ src/main/java/com/tubes2_btc/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -12,7 +13,9 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Pages/main-page.fxml"));
 //      Parent root = FXMLLoader.load(getClass().getResource("Pages/load.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("Pages/Images/AddOns/logo.jpg")));
         primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Harvest BTC Nolan");
         primaryStage.show();
     }
 
